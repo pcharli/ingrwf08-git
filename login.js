d89d54d35cc5cbfd4c06e7fe8871ba7543a3ac47
Mathieu <mathieu.slaedts@generis.be>
2020-06-26 14:35:32 +0200

    Remove old file

--- login.js
+++
@@ -1,9 +0,0 @@
-const loginForm = document.querySelector(".login-form");
-loginForm.addEventListener("click", ev => logIn(ev));
-
-const logIn = ev => {
-    if(ev.target.tagName !== "BUTTON") return;
-    const pseudo = loginForm.querySelector(".login-form__login-input");
-    alert(pseudo.value + " est connecté");
-};
-
